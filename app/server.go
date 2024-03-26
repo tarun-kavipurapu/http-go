57d316e35b58f5ccd22ab82dfeed69ad48de58ed
Tarun <tarunsharmakavipurapu@gmail.com>
2024-03-26 09:07:14 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -9,6 +9,21 @@ import (
 	// "os"
 )
 
+func handleConnection(conn net.Conn) {
+
+	defer conn.Close()
+
+	response := []byte("HTTP/1.1 200 OK\r\n\r\n")
+
+	_, err := conn.Write(response)
+
+	if err != nil {
+		fmt.Println("Error writing:", err)
+		return
+	}
+
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -22,10 +37,13 @@ func main() {
 	}
 	defer l.Close()
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to Accept the connection")
+		}
+		handleConnection(conn)
+
 	}
 
 }
